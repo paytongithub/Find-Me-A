7cdf542ee594c4792111ebf6815ce5a8433d083f
Layla Payton <trp31604@gmail.com>
2026-03-29 18:45:04 -0500

    added a method for search queries, barebones rn but will be updated in the future. also added the overhead class in connection.cs for right now, this will be moved later to whatever class contains the search function

--- Find Me A/Connection.cs
+++ Find Me A/Connection.cs
@@ -4,6 +4,18 @@ using System.Data.SqlClient;
 
 namespace Find_Me_A
 {
+    // not sure where to put this atm, but this is the overhead needed to access the searches
+    public class Title
+    {
+        public string TitleName { get; set; }
+        public string TitleType { get; set; } // Movie / TV
+        public DateTime ReleaseDate { get; set; }
+        public decimal AverageRating { get; set; }
+        public int NumberOfRatings { get; set; }
+        public int? EpisodeCount { get; set; } // null for movies
+        public List<string> Genres { get; set; } = new List<string>();
+        public List<string> Actors { get; set; } = new List<string>();
+    }
     class Connection
     {
         static void Main(string[] args)
---
+++ Find Me A/Search.cs
@@ -0,0 +1,251 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Net.WebRequestMethods;
+
+namespace Find_Me_A
+{
+    public class Search
+    {
+        private readonly string _connectionString;
+
+        public Search(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private List<Title> ExecuteTitleQuery(string sql, params SqlParameter[] parameters)
+        {
+            var result = new List<Title>();
+
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new SqlCommand(sql, conn))
+                {
+                    if (parameters != null)
+                        cmd.Parameters.AddRange(parameters);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var title = new Title
+                            {
+                                TitleName = reader["TitleName"].ToString(),
+                                TitleType = reader["TitleType"].ToString(),
+                                ReleaseDate = Convert.ToDateTime(reader["ReleaseDate"]),
+                                AverageRating = Convert.ToDecimal(reader["AverageRating"]),
+                                NumberOfRatings = Convert.ToInt32(reader["NumberOfRatings"]),
+                                EpisodeCount = reader["EpisodeCount"] == DBNull.Value ? (int?)null : Convert.ToInt32(reader["EpisodeCount"]),
+                                Genres = new List<string>(reader["Genres"].ToString().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)),
+                                Actors = new List<string>(reader["Actors"].ToString().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries))
+                            };
+
+                            result.Add(title);
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public List<Title> SearchByTitle(string titleName)
+        {
+            string sql = @"
+                SELECT 
+                    t.TitleName,
+                    t.TitleType,
+                    t.ReleaseDate,
+                    t.Rating AS AverageRating,
+                    t.NumberOfRatings,
+                    ts.EpisodeCount,
+                    (SELECT STRING_AGG(g2.GenreName, ', ')
+                     FROM TitleGenres tg2
+                     JOIN Genres g2 ON tg2.GenreID = g2.GenreID
+                     WHERE tg2.TitleID = t.TitleID) AS Genres,
+                    (SELECT STRING_AGG(a2.ActorName, ', ')
+                     FROM TitleActors ta2
+                     JOIN Actors a2 ON ta2.ActorID = a2.ActorID
+                     WHERE ta2.TitleID = t.TitleID) AS Actors
+                FROM Titles t
+                LEFT JOIN TVShows ts ON t.TitleID = ts.TitleID
+                WHERE t.TitleName LIKE '%' + @TitleName + '%';";
+
+            return ExecuteTitleQuery(sql, new SqlParameter("@TitleName", titleName));
+        }
+
+        public List<Title> SearchByGenre(string genreName)
+        {
+            string sql = @"
+                SELECT DISTINCT
+                    t.TitleName,
+                    t.TitleType,
+                    t.ReleaseDate,
+                    t.Rating AS AverageRating,
+                    t.NumberOfRatings,
+                    ts.EpisodeCount,
+                    (SELECT STRING_AGG(g2.GenreName, ', ')
+                     FROM TitleGenres tg2
+                     JOIN Genres g2 ON tg2.GenreID = g2.GenreID
+                     WHERE tg2.TitleID = t.TitleID) AS Genres,
+                    (SELECT STRING_AGG(a2.ActorName, ', ')
+                     FROM TitleActors ta2
+                     JOIN Actors a2 ON ta2.ActorID = a2.ActorID
+                     WHERE ta2.TitleID = t.TitleID) AS Actors
+                FROM Titles t
+                LEFT JOIN TVShows ts ON t.TitleID = ts.TitleID
+                JOIN TitleGenres tg ON t.TitleID = tg.TitleID
+                JOIN Genres g ON tg.GenreID = g.GenreID
+                WHERE g.GenreName = @GenreName;";
+
+            return ExecuteTitleQuery(sql, new SqlParameter("@GenreName", genreName));
+        }
+
+        public List<Title> SearchByActor(string actorName)
+        {
+            string sql = @"
+                SELECT DISTINCT
+                    t.TitleName,
+                    t.TitleType,
+                    t.ReleaseDate,
+                    t.Rating AS AverageRating,
+                    t.NumberOfRatings,
+                    ts.EpisodeCount,
+                    (SELECT STRING_AGG(g2.GenreName, ', ')
+                     FROM TitleGenres tg2
+                     JOIN Genres g2 ON tg2.GenreID = g2.GenreID
+                     WHERE tg2.TitleID = t.TitleID) AS Genres,
+                    (SELECT STRING_AGG(a2.ActorName, ', ')
+                     FROM TitleActors ta2
+                     JOIN Actors a2 ON ta2.ActorID = a2.ActorID
+                     WHERE ta2.TitleID = t.TitleID) AS Actors
+                FROM Titles t
+                LEFT JOIN TVShows ts ON t.TitleID = ts.TitleID
+                JOIN TitleActors ta ON t.TitleID = ta.TitleID
+                JOIN Actors a ON ta.ActorID = a.ActorID
+                WHERE a.ActorName = @ActorName;";
+
+            return ExecuteTitleQuery(sql, new SqlParameter("@ActorName", actorName));
+        }
+
+        // overload method for multiple genres 
+        public List<Title> SearchByGenre(string[] genreNames)
+        {
+            if (genreNames == null || genreNames.Length == 0)
+                return new List<Title>();
+
+            var paramNames = new List<string>();
+            var parameters = new List<SqlParameter>();
+            for (int i = 0; i < genreNames.Length; i++)
+            {
+                string param = "@Genre" + i;
+                paramNames.Add(param);
+                parameters.Add(new SqlParameter(param, genreNames[i]));
+            }
+
+            string sql = $@"
+                SELECT DISTINCT
+                    t.TitleName,
+                    t.TitleType,
+                    t.ReleaseDate,
+                    t.Rating AS AverageRating,
+                    t.NumberOfRatings,
+                    ts.EpisodeCount,
+                    (SELECT STRING_AGG(g2.GenreName, ', ')
+                     FROM TitleGenres tg2
+                     JOIN Genres g2 ON tg2.GenreID = g2.GenreID
+                     WHERE tg2.TitleID = t.TitleID) AS Genres,
+                    (SELECT STRING_AGG(a2.ActorName, ', ')
+                     FROM TitleActors ta2
+                     JOIN Actors a2 ON ta2.ActorID = a2.ActorID
+                     WHERE ta2.TitleID = t.TitleID) AS Actors
+                FROM Titles t
+                LEFT JOIN TVShows ts ON t.TitleID = ts.TitleID
+                JOIN TitleGenres tg ON t.TitleID = tg.TitleID
+                JOIN Genres g ON tg.GenreID = g.GenreID
+                WHERE g.GenreName IN ({string.Join(", ", paramNames)});";
+
+            return ExecuteTitleQuery(sql, parameters.ToArray());
+        }
+        // overload method for multiple actors
+        public List<Title> SearchByActor(string[] actorNames)
+        {
+            if (actorNames == null || actorNames.Length == 0)
+                return new List<Title>();
+
+            var paramNames = new List<string>();
+            var parameters = new List<SqlParameter>();
+            for (int i = 0; i < actorNames.Length; i++)
+            {
+                string param = "@Actor" + i;
+                paramNames.Add(param);
+                parameters.Add(new SqlParameter(param, actorNames[i]));
+            }
+
+            string sql = $@"
+                SELECT DISTINCT
+                    t.TitleName,
+                    t.TitleType,
+                    t.ReleaseDate,
+                    t.Rating AS AverageRating,
+                    t.NumberOfRatings,
+                    ts.EpisodeCount,
+                    (SELECT STRING_AGG(g2.GenreName, ', ')
+                     FROM TitleGenres tg2
+                     JOIN Genres g2 ON tg2.GenreID = g2.GenreID
+                     WHERE tg2.TitleID = t.TitleID) AS Genres,
+                    (SELECT STRING_AGG(a2.ActorName, ', ')
+                     FROM TitleActors ta2
+                     JOIN Actors a2 ON ta2.ActorID = a2.ActorID
+                     WHERE ta2.TitleID = t.TitleID) AS Actors
+                FROM Titles t
+                LEFT JOIN TVShows ts ON t.TitleID = ts.TitleID
+                JOIN TitleActors ta ON t.TitleID = ta.TitleID
+                JOIN Actors a ON ta.ActorID = a.ActorID
+                WHERE a.ActorName IN ({string.Join(", ", paramNames)});";
+
+            return ExecuteTitleQuery(sql, parameters.ToArray());
+        }
+
+        // switch for any of the methods
+        public void SearchQuery(string SearchBy, string Data)
+        {
+            switch (SearchBy)
+            {
+                case "Title":
+                    SearchByTitle(Data);
+                    break;
+                case "Genre":
+                    SearchByGenre(Data);
+                    break;
+                case "Actor":
+                    SearchByActor(Data);
+                    break;
+                default:
+                    Console.WriteLine("Invalid search criteria.");
+                    break;
+            }
+        }
+
+        // overload method for searchquery switch to work with array data
+        public void SearchQuery(string SearchBy, string[] Data)
+        {
+            switch (SearchBy)
+            {
+                case "Genre":
+                    SearchByGenre(Data);
+                    break;
+                case "Actor":
+                    SearchByActor(Data);
+                    break;
+                default:
+                    Console.WriteLine("Invalid search criteria.");
+                    break;
+            }
+        }
+    }
+}
