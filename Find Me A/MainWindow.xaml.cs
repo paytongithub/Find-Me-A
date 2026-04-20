5151ad6f3eced24814964acd08bce181637328c7
gtrujillo1 <157073326+gtrujillo1@users.noreply.github.com>
2026-04-19 21:58:30 -0500

    Sign up & Log in

---
+++ Find Me A/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FindMeA_.Views;
+
+namespace FindMeA_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new WelcomePage());
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new HomePage());
+        private void Search_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new SearchPage());
+        private void Watchlist_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new WatchlistPage());
+
+        private void Reviews_Click(object sender, RoutedEventArgs e)
+            => MainFrame.Navigate(new ReviewsPage());
+
+        private void Profile_Click(object sender, RoutedEventArgs e)
+             => MainFrame.Navigate(new ProfilePage());
+    }
+}
---
+++ Find Me A/SignUpPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FindMeA_.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FindMeA_
+{
+    /// <summary>
+    /// Interaction logic for SignUpPage.xaml
+    /// </summary>
+    public partial class SignUpPage : Page
+    {
+        public SignUpPage()
+        {
+            InitializeComponent();
+        }
+        private void SignUpSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Account created successfully!");
+
+           ///Navigate to homepage
+            NavigationService.Navigate(new HomePage());
+        }
+
+        private void GoToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new LogInPage());
+        }
+    }
+}
