1ea13af7b0aba20e656dcd3b40a7d357569b690b
gtrujillo1 <157073326+gtrujillo1@users.noreply.github.com>
2026-04-19 21:54:01 -0500

    Frontend

---
+++ Find Me A/HomePage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FindMeA_.Views
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            //Image paths need to be inserted
+            FeaturedItems.ItemsSource = new string[]
+            {
+
+            };
+            TopPicksItems.ItemsSource = new string[]
+            {
+
+            };
+            TrendingItems.ItemsSource = new string[]
+            {
+
+            };
+
+
+        }
+    }
+}
