a26887a1d3e84a0a53c766ad1766efba2fedbb41
Layla Payton <trp31604@gmail.com>
2026-04-28 17:39:21 -0500

    finalizing connection for final

--- Find Me A/Connection.cs
+++ Find Me A/Connection.cs
@@ -23,28 +23,5 @@ namespace Find_Me_A
     }
     class Connection
     {
-        /*
-        static void Main(string[] args)
-        {
-            // below is test code for adding to the database, for layla's account
-            string connectionString = "Server=147.126.2.58;Database=Find_Me_A;User ID=tpayton1;Password=p22817;TrustServerCertificate=True;";
-            WatchList watchList = new WatchList(connectionString);
-
-            // Add a movie to Layla's watch list
-            watchList.AddToWatchList("Layla", "Test Film", new DateTime(2026, 3, 15), 7);
-
-            // Update the rating
-            watchList.UpdateRating("Layla", "Test Film", 8);
-
-            // Remove a title
-            watchList.RemoveFromWatchList("Layla", "Test Film");
-        }
-        
-        static void Main2(string[] args)
-        {
-            Console.WriteLine("Server running...");
-        }
-    }
-        */
         }
 }
